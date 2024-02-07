39644a241ae003979647ca12865b0748f281c76c
Hermes <hermessantos995@gmail.com>
2024-02-07 20:19:01 -0300

    add current PHP version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use regex::Regex;
 use std::env;
 
 fn main() {
-    let valid_args: [&str; 8] = [
+    let valid_args: [&str; 10] = [
         "--list",
         "--help",
+        "--current",
+        "-cv",
         "-h",
         "use <php_version>",
         "-i",
@@ -11,6 +14,7 @@ fn main() {
         "--remove",
         "-r",
     ];
+    let version: &str = "1.0.0";
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
@@ -21,7 +25,8 @@ fn main() {
 
     if valid_args.contains(&first_arg.as_str()) {
         match first_arg.as_str() {
-            "--help" | "-h" => show_help(),
+            "--help" | "-h" => show_help(&version),
+            "-cv" | "--current" => current_php_v(),
             _ => invalid_argument_given(),
         }
     } else {
@@ -31,8 +36,31 @@ fn main() {
 fn invalid_argument_given() {
     println!("Invalid argument given, for a list of valid arguments type pvm --help or pvm -h");
 }
-fn show_help() {
+fn show_help(version: &str) {
     println!(
-        "PVM running version v1.0.0\nSupported arguments: \n --list, --help, -h, use <php_version>, -i, --install, -r, --remove"
+        "PVM running version v{}\nSupported arguments: \n --list, --help, -h, use <php_version>, -i, --install, -r, --remove",
+        version
     );
+}
+fn current_php_v() {
+    if let Ok(path) = env::var("path") {
+        let parts: Vec<&str> = path.split(';').collect();
+        for part in parts {
+            if part.contains("wamp64\\bin\\php\\") {
+                let re = Regex::new(r"php(\d+\.\d+\.\d+)$").unwrap();
+
+                // Verifica se há correspondência na string
+                if let Some(capturas) = re.captures(part) {
+                    // Extrai a parte correspondente à versão
+                    if let Some(versao) = capturas.get(1) {
+                        println!("Current PHP running: {}", versao.as_str());
+                    }
+                } else {
+                    println!("No PHP versions found on path");
+                }
+            }
+        }
+    } else {
+        println!("erro")
+    }
 }
