ea747510c16f2a4c3b957262ba84e428edad035b
Hermes <hermessantos995@gmail.com>
2024-02-08 01:39:32 -0300

    add the list php versions command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
 use std::env;
+use std::fs;
 
 fn main() {
+    let php_dir: &str = "C:\\wamp64\\bin\\php";
     let valid_args: [&str; 9] = [
         "--list",
         "--help",
@@ -26,6 +28,7 @@ fn main() {
         match first_arg.as_str() {
             "--help" | "-h" => show_help(&version),
             "-cv" | "--current" => current_php_v(),
+            "--list" => list_php_versions(&php_dir),
             _ => invalid_argument_given(),
         }
     } else {
@@ -62,16 +65,16 @@ fn show_help(version: &str) {
         "-r <version>", "Remove the specified PHP version"
     );
 }
+
 fn current_php_v() {
     if let Ok(path) = env::var("path") {
         let parts: Vec<&str> = path.split(';').collect();
+
         for part in parts {
             if part.contains("wamp64\\bin\\php\\") {
                 let re = Regex::new(r"php(\d+\.\d+\.\d+)$").unwrap();
 
-                // Verifica se há correspondência na string
                 if let Some(capturas) = re.captures(part) {
-                    // Extrai a parte correspondente à versão
                     if let Some(versao) = capturas.get(1) {
                         println!("Current PHP running: {}", versao.as_str());
                     }
@@ -82,5 +85,52 @@ fn current_php_v() {
         }
     } else {
         println!("erro")
+    }
+}
+
+fn return_current_php_v() -> String {
+    if let Ok(path) = env::var("path") {
+        let parts: Vec<&str> = path.split(';').collect();
+
+        for part in parts {
+            if part.contains("wamp64\\bin\\php\\") {
+                let re = Regex::new(r"php(\d+\.\d+\.\d+)$").unwrap();
+
+                if let Some(capturas) = re.captures(part) {
+                    if let Some(versao) = capturas.get(1) {
+                        return versao.as_str().to_string();
+                    }
+                } else {
+                    return "Error".to_string();
+                }
+            }
+        }
+        return "Error".to_string();
+    } else {
+        return "Error".to_string();
+    }
+}
+
+fn list_php_versions(path: &str) {
+    let paths = fs::read_dir(path).unwrap();
+    let re = Regex::new(r"php(\d+\.\d+\.\d+)$").unwrap();
+    let php_v = return_current_php_v();
+
+    println!("Instaled PHP versions\n");
+
+    for path in paths {
+        if let Ok(entry) = path {
+            if let Some(file_name) = entry.file_name().into_string().ok() {
+                if let Some(captures) = re.captures(&file_name) {
+                    if let Some(version) = captures.get(1) {
+                        if version.as_str() == php_v {
+                            println!("{} * (Currently using)", version.as_str());
+                        } else {
+                            println!("{}", version.as_str());
+                        }
+                    }
+                }
+            }
+        }
     }
 }
