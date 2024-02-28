21dbafb73bc95155ce9c5627e93845f8f9d4bcb0
Hermes <hermessantos995@gmail.com>
2024-02-28 16:21:03 -0300

    adds the switch php version with no real implementations: debug only

--- src/main.rs
+++ src/main.rs
@@ -29,34 +29,78 @@ fn main() {
             "--help" | "-h" => show_help(&version),
             "-cv" | "--current" => current_php_v(),
             "--list" => list_php_versions(&php_dir),
-            "--use" => switch_php_version(&args),
+            "--use" => switch_php_version(&args[2]),
             _ => invalid_argument_given(),
         }
     } else {
         invalid_argument_given()
     }
 }
-fn switch_php_version(arg: &Vec<String>) {
+fn switch_php_version(arg: &str) {
+    let php_dir: &str = "C:\\wamp64\\bin\\php";
     if arg.len() < 3 {
         println!("PHP version was not given.\nYou can type pvm -h or --help to look for details.")
+    } else {
+        if verify_arg(&arg, &php_dir) == true {
+            let full_path_env: String = env::var("path").unwrap();
+            let re = Regex::new(r"php(\d+\.\d+\.\d+)$").unwrap();
+            let path_to_be_modified = return_current_path();
+            if let Some(php_number_version) = re.captures(&path_to_be_modified) {
+                if let Some(php_last) = php_number_version.get(1) {
+                    let new_path = path_to_be_modified.replace(php_last.as_str(), arg);
+                    let new_full_path = full_path_env.replace(&path_to_be_modified, &new_path);
+                    // here the PVM actually changes the full path
+                    println!("{:?}", new_full_path);
+                }
+            }
+        } else {
+            println!("PHP version especified is not installed.\nRun pvm --i <php_version_desired> if you wish to install this php version")
+        }
     }
 }
+
+fn verify_arg(arg_given: &str, path: &str) -> bool {
+    let re = Regex::new(r"php(\d+\.\d+\.\d+)$").unwrap();
+
+    if let Ok(paths) = fs::read_dir(path) {
+        for path in paths {
+            if let Ok(entry) = path {
+                if let Some(file_name) = entry.file_name().into_string().ok() {
+                    if let Some(captures) = re.captures(&file_name) {
+                        if let Some(version) = captures.get(1) {
+                            if version.as_str() == arg_given {
+                                // Todo: create a struck to handle this
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    return false;
+}
+
 fn invalid_argument_given() {
     println!("Invalid argument given. For a list of valid arguments type pvm --help or pvm -h");
 }
 fn show_help(version: &str) {
     println!("PVM running version v{}\n", version);
-    println!("{:<20} : {}", "--list", "List all installed PHP versions");
-    println!("{:<20} : {}", "--help", "Show help");
-    println!("{:<20} : {}", "-h", "Show help");
-    println!("{:<20} : {}", "--current", "Show current PHP running");
     println!(
         "{:<20} : {}",
-        "use <php_version>", "Switch to the specified PHP version"
+        "--list", "List all installed PHP versions (ok)"
+    );
+    println!("{:<20} : {}", "--help", "Show help (ok)");
+    println!("{:<20} : {}", "-h", "Show help (ok)");
+    println!("{:<20} : {}", "--current", "Show current PHP running (ok)");
+    println!(
+        "{:<20} : {}",
+        "--use <php_version>", "Switch to the specified PHP version"
     );
     println!(
         "{:<20} : {}",
-        "-i <version>", "Switch to the specified PHP version"
+        "-i <version>", "Install the specified PHP version"
     );
     println!(
         "{:<20} : {}",
@@ -70,6 +114,18 @@ fn show_help(version: &str) {
         "{:<20} : {}",
         "-r <version>", "Remove the specified PHP version"
     );
+}
+
+fn return_current_path() -> String {
+    if let Ok(path) = env::var("path") {
+        let paths: Vec<&str> = path.split(';').collect();
+        for sub_path in paths {
+            if sub_path.contains("wamp64\\bin\\php") {
+                return sub_path.to_string();
+            }
+        }
+    }
+    return "".to_string();
 }
 
 fn current_php_v() {
